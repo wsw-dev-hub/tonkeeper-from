e9abfdd0fe13a2730a0667bd770e1683ea27d040
wsw-dev-hub <wsw.contato@gmail.com>
2025-10-20 13:03:53 -0300

    Add tonconnect-manifest.json and project files

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ import { TonConnectUI } from '@tonconnect/ui';
 
 document.addEventListener('DOMContentLoaded', () => {
   const tonConnectUI = new TonConnectUI({
-    manifestUrl: 'https://raw.githubusercontent.com/ton-connect/demo-dapp/main/tonconnect-manifest.json'
+    /*manifestUrl: 'https://raw.githubusercontent.com/ton-connect/demo-dapp/main/tonconnect-manifest.json'
+    manifestUrl: 'https://github.com/wsw-dev-hub/tonkeeper-from/blob/main/tonconnect-manifest.json'*/
+    manifestUrl: 'https://wsw-dev-hub.github.io/tonkeeper-form/tonconnect-manifest.json',
+    network: 'testnet'
   });
 
   const form = document.getElementById('tonForm');
